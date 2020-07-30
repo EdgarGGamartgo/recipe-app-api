1707f2977560022af8bac17d93382ff5b9f00f43
EdgarGGamartgo <43557626+EdgarGGamartgo@users.noreply.github.com>
2020-07-30 00:55:52 -0500

    Token Authentication

--- app/user/urls.py
+++ app/user/urls.py
@@ -6,4 +6,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
+    path('create/superuser', views.CreateSuperUserView.as_view(), name='createsuperuser'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
 ]
